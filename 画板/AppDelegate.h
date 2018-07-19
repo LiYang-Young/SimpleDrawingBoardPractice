119ff345782e32163eb56502bd89360ebb95649a
douliyangwan <liyang7947@sina.com>
2018-07-19 14:28:12 +0800

    Initial Commit

---
+++ 画板/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  画板
+//
+//  Created by 李洋 on 2018/7/19.
+//  Copyright © 2018年 com.appTest.app. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 画板/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  画板
+//
+//  Created by 李洋 on 2018/7/19.
+//  Copyright © 2018年 com.appTest.app. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
